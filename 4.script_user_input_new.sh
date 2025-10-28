5e677778fda41a3e72d36664bf7d9d14d9010cc8
venkian96 <venkatesh.a1415@gmail.com>
2025-10-28 12:08:40 +0530

    modifies user input sript

--- 4.script_user_input_new.sh
+++ 4.script_user_input_new.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 read -p "please enter AWS region name to search  for vpc:" REGION
 echo "the given region by user is" ${$REGION}
-aws ec2 describe-vpcs --region ${REGION}
+aws ec2 describe-vpcs --region ${REGION
  
