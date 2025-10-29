380c73fe5726a4d24cad6c1b50bacea30bb52ab7
venkian96 <venkatesh.a1415@gmail.com>
2025-10-29 20:40:56 +0530

    adding multi args

--- 10.start_nginx.sh
+++ 10.start_nginx.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 SERVICE=$1
-service $1 status
+service $1 status > /dev/null
 if [ $? -eq 0]
 then
    echo "The service $SERVICE is already running"
 else
-    service $1 start
+    service $1 start > /dev/null
     echo "started $SERVICE successfully"
 fi
