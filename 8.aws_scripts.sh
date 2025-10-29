fcc48349999959c99a7ee67e6bc9900e6f88f7f8
venkian96 <venkatesh.a1415@gmail.com>
2025-10-29 17:53:18 +0530

    testing aws

--- 8.aws_scripts.sh
+++ 8.aws_scripts.sh
@@ -4,8 +4,8 @@ REGION=$1
 REGION=$2
 REGION=$3
 echo "Lets get Vpc info for ${REGION1} ..."
-aws ec2 describe-vpcs --region $1 | jq ".Vpcs[].VpcId" -r
+aws ec2 describe-vpcs --region $REGION1 | jq ".Vpcs[].VpcId" -r
 echo "Lets get Vpc info for ${REGION2} ..."
-aws ec2 describe-vpcs --region $2 | jq ".Vpcs[].VpcId" -r
+aws ec2 describe-vpcs --region $REGION2 | jq ".Vpcs[].VpcId" -r
 echo "Lets get Vpc info for ${REGION3} ..."
-aws ec2 describe-vpcs --region $3 | jq ".Vpcs[].VpcId" -r
+aws ec2 describe-vpcs --region $REGION3 | jq ".Vpcs[].VpcId" -r
