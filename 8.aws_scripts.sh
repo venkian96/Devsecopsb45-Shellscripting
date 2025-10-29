4ff809c85f9746b899d646c1f6038538820fcc69
venkian96 <venkatesh.a1415@gmail.com>
2025-10-29 17:46:02 +0530

    testing aws

--- 8.aws_scripts.sh
+++ 8.aws_scripts.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "You have given $# Args to this script"
 REGION=$1
 REGION=$2
+echo "Lets get Vpc info for ${REGION1} ..."
 aws ec2 describe-vpcs --region $1 | jq ".Vpcs[].VpcId" -r
 aws ec2 describe-vpcs --region $2 | jq ".Vpcs[].VpcId" -r
