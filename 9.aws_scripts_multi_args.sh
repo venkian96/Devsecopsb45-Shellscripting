1e72f0c2b01ff962ebfc4e74ebb2f07b469a19e2
venkian96 <venkatesh.a1415@gmail.com>
2025-10-29 18:45:19 +0530

    adding multi args

--- 9.aws_scripts_multi_args.sh
+++ 9.aws_scripts_multi_args.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+echo 'Testing $@'
 if [ $# -gt 0 ]; then
 for REGION in $@;
+  do
+    echo "Lets get Vpc info for ${REGION} ..."
+    aws ec2 describe-vpcs --region ${REGION} | jq ".Vpcs[].VpcId" -r
+  done
+else
+  echo "You have given $# args to this script.Please provide atlest one region"
+fi
+
+echo '-----------------------------------------------------------------------------'
+echo 'Testing $*'
+if [ $# -gt 0 ]; then
+for REGION in $*;
   do
     echo "Lets get Vpc info for ${REGION} ..."
     aws ec2 describe-vpcs --region ${REGION} | jq ".Vpcs[].VpcId" -r
